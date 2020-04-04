a060eed237db8ca9b02cc9e9f175fcbaefd9e84c
Greta Mezzina <gretamezzina@Air-di-Greta>
2020-04-04 16:25:20 +0200

    sent message alert added on send button

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ function switchTheme(event) {
 
 const toggler = document.querySelector('#toggler');
 toggler.addEventListener('change', switchTheme, false);
+
+
+
+function messageSent() {
+    const message = 'Hello your message has been sent, I will get back to you as soon as possible'
+    alert(message);
+  }
+
